5632e08acfa47871b5761900c5e829e0fcfa752f
Fabrice <fabrice.dore@gmail.com>
2020-05-07 14:30:50 +0100

    correct ws middleware initialization

--- src/core/transport/middleware.ts
+++ src/core/transport/middleware.ts
@@ -3,23 +3,25 @@ import { AppActions } from 'modules/app/actions';
 import { Connection } from './Connection';
 import { TransportActions } from './actions';
 
-const createWsMiddleware = ({ connection }: { connection: Connection }): Middleware => store => (next: Dispatch) => (action: AppActions) => {
-    connection.onReceive(data => {
-        const parsedData = JSON.parse(data);
-        let meta = undefined;
-        let channelId = undefined;
+const createWsMiddleware = ({ connection }: { connection: Connection }): Middleware => {
+    return store => (next: Dispatch) => {
+        connection.onReceive(data => {
+            const parsedData = JSON.parse(data);
+            let meta = undefined;
+            let channelId = undefined;
 
-        if (Array.isArray(parsedData)) {
-            channelId = parsedData[0];
-        } else if (parsedData.hasOwnProperty('chanId')) {
-            channelId = parsedData.chanId;
-        }
-        if (channelId && store.getState().subscriptions[channelId]) {
-            meta = store.getState().subscriptions[channelId];
-        }
-        next(TransportActions.receiveMessage(parsedData, meta));
-    });
-    return next(action);
+            if (Array.isArray(parsedData)) {
+                channelId = parsedData[0];
+            } else if (parsedData.hasOwnProperty('chanId')) {
+                channelId = parsedData.chanId;
+            }
+            if (channelId && store.getState().subscriptions[channelId]) {
+                meta = store.getState().subscriptions[channelId];
+            }
+            next(TransportActions.receiveMessage(parsedData, meta));
+        });
+        return (action: AppActions) => next(action);
+    };
 }
 
 export default createWsMiddleware;
