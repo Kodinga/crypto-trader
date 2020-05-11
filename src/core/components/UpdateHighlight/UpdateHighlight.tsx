335dd42d4029dff02aa9912e3b34e6b3ea6d1f91
Fabrice <fabrice.dore@gmail.com>
2020-05-11 19:37:51 +0100

    refactor highlight animation

--- src/core/components/UpdateHighlight/UpdateHighlight.tsx
+++ src/core/components/UpdateHighlight/UpdateHighlight.tsx
@@ -1,117 +1,114 @@
 import React, { FC, useEffect, useState, createRef } from "react";
 import { usePrevious } from "core/hooks/usePrevious";
 import {
-  Content,
-  IdenticalPart,
-  DigitContainer,
+    Content,
+    IdenticalPart,
+    DigitContainer,
 } from "./UpdateHightlight.styled";
 import Palette from "theme/style";
 
 export interface IProps {
-  value?: string | null;
-  effect?: "zoom" | "default";
+    value?: string | null;
+    effect?: "zoom" | "default";
 }
 
 export const calculateParts = (value: string, prevValue: string) => {
-  if (!value || !prevValue) {
-    return [value, ""];
-  }
-  const length = Math.min(value.length, prevValue.length);
-  let index = 0;
-  for (let i = 0; i < length; i++) {
-    if (value[i] === prevValue[i]) {
-      index++;
-    } else {
-      break;
+    if (!value || !prevValue) {
+        return [value, ""];
     }
-  }
-  return [value.slice(0, index), value.slice(index, value.length)];
+    const length = Math.min(value.length, prevValue.length);
+    let index = 0;
+    for (let i = 0; i < length; i++) {
+        if (value[i] === prevValue[i]) {
+            index++;
+        } else {
+            break;
+        }
+    }
+    return [value.slice(0, index), value.slice(index, value.length)];
 };
 
-const Digit: FC<{
-  value: string;
-  delay?: number;
-  scale?: number;
-  duration?: number;
+const AnimatedValue: FC<{
+    value: string;
+    delay?: number;
+    scale?: number;
+    duration?: number;
 }> = (props) => {
-  const { value, delay = 0, scale = 1, duration = 200 } = props;
-  const ref = createRef<HTMLDivElement>();
+    const { value, delay = 0, scale = 1, duration = 200 } = props;
+    const ref = createRef<HTMLDivElement>();
 
-  useEffect(() => {
-    let animation: Animation | undefined = undefined;
-    let timeoutId: number | undefined = undefined;
+    useEffect(() => {
+        let animation: Animation | undefined = undefined;
+        let timeoutId: number | undefined = undefined;
 
-    if (typeof ref.current?.animate === "function") {
-      const runAnimation = () => {
-        animation = ref.current?.animate(
-          [
-            // keyframes
-            { color: Palette.Orange, transform: "scale(1)" },
-            ...(scale === 1
-              ? []
-              : [{ color: Palette.White, transform: `scale(${scale})` }]),
-            { color: Palette.White, transform: "scale(1)" },
-          ],
-          {
-            duration,
-            iterations: 1,
-          }
-        );
-      };
+        if (typeof ref.current?.animate === "function") {
+            const runAnimation = () => {
+                animation = ref.current?.animate(
+                    [
+                        // keyframes
+                        { color: Palette.Orange, transform: "scale(1)" },
+                        ...(scale === 1
+                            ? []
+                            : [{ color: Palette.White, transform: `scale(${scale})` }]),
+                        { color: Palette.White, transform: "scale(1)" },
+                    ],
+                    {
+                        duration,
+                        iterations: 1,
+                    }
+                );
+            };
 
-      if (delay) {
-        timeoutId = setTimeout(
-          () => requestAnimationFrame(runAnimation),
-          delay
-        );
-      } else {
-        requestAnimationFrame(runAnimation);
-      }
-    }
+            if (delay) {
+                timeoutId = setTimeout(
+                    () => requestAnimationFrame(runAnimation),
+                    delay
+                );
+            } else {
+                requestAnimationFrame(runAnimation);
+            }
+        }
 
-    return () => {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-      if (animation) {
-        animation.cancel();
-      }
-    };
+        return () => {
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+            if (animation) {
+                animation.cancel();
+            }
+        };
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [value]);
 
-  return <DigitContainer ref={ref}>{value}</DigitContainer>;
+    return <DigitContainer ref={ref}>{value}</DigitContainer>;
 };
 
 const UpdateHighlight: FC<IProps> = (props) => {
-  const changedPartRef = createRef<HTMLDivElement>();
-  const { value, effect = "default" } = props;
-  const prev = usePrevious(value);
-  const [[identicalPart, changedPart], setParts] = useState<string[]>([]);
+    const changedPartRef = createRef<HTMLDivElement>();
+    const { value, effect = "default" } = props;
+    const prev = usePrevious(value);
+    const [[identicalPart, changedPart], setParts] = useState<string[]>([]);
 
-  useEffect(() => {
-    setParts(calculateParts(value || "", prev || ""));
+    useEffect(() => {
+        setParts(calculateParts(value || "", prev || ""));
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [value]);
 
-  return (
-    <Content>
-      <IdenticalPart>{identicalPart}</IdenticalPart>
-      <div ref={changedPartRef}>
-        {changedPart?.split("").map((digit, index) => (
-          <Digit
-            key={index}
-            value={digit}
-            delay={effect === "zoom" ? index * 100 : undefined}
-            scale={effect === "zoom" ? 2 : undefined}
-            duration={effect === "zoom" ? 300 : undefined}
-          />
-        ))}
-      </div>
-    </Content>
-  );
+    const digits = effect === "zoom"
+        ? changedPart?.split("").map((digit, index) => (
+            <AnimatedValue key={index} value={digit} delay={index * 100} scale={2} duration={300} />
+        ))
+        : <AnimatedValue value={changedPart} />
+    return (
+        <Content>
+            <IdenticalPart>{identicalPart}</IdenticalPart>
+            <div ref={changedPartRef}>
+                {digits}
+            </div>
+        </Content>
+    );
 };
 
 export default UpdateHighlight;
