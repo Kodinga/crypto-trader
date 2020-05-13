d7411a5eb6b9aac414b81c2cf147179319161a0d
Fabrice <fabrice.dore@gmail.com>
2020-05-13 11:06:33 +0100

    fix memoization key

--- src/modules/candles/selectors.ts
+++ src/modules/candles/selectors.ts
@@ -2,12 +2,16 @@ import { createSelector } from "reselect";
 import { memoize } from "lodash";
 import { RootState } from "modules/root";
 import { getLookupKey } from "./utils";
+import { Candle } from "./types/Candle";
+
+const DEFAULT_CANDLES: Candle[] = [];
 
 export const candlesSelector = (state: RootState) => state.candles;
 
 export const getCandles = createSelector(candlesSelector, (candles) =>
   memoize(
     (currencyPair: string, timeframe: string) =>
-      candles[getLookupKey(currencyPair, timeframe)]
+      candles[getLookupKey(currencyPair, timeframe)] || DEFAULT_CANDLES,
+    (currencyPair, timeframe) => `${currencyPair}:${timeframe}`
   )
 );
